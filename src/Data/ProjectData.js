0ed7af27ca9d2e2e5791e64b973df1e6ba3e1d7f
Farhan akhtar <sheikhfarhan722@gmail.com>
2022-12-11 01:19:00 +0530

    Project page corrected

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,12 +1,42 @@
-const projData = [{
-    card_Img: "assets/Proj_img/temp.jpg",
-    proj_Name: "tempName",
-    proj_Desc: "tempDesc",
-    techStack1: "html",
-    techStack2: "css",
-    techStack3: "java",
-    techStack4: "",
-    techStack5: "",
-}]
+const projData = [
+    {
+        card_Img: "assets/Proj_img/temp.jpg",
+        proj_Link: "https://www.example.com",
+        proj_Name: "tempName",
+        proj_Desc: "tempDesc",
+        techStack1: "html",
+        techStack2: "css",
+        techStack3: "java",
+        techStack4: "",
+        techStack5: "",
+    }, {
+        card_Img: "assets/Proj_img/temp.jpg",
+        proj_Name: "tempName",
+        proj_Desc: "tempDesc",
+        techStack1: "html",
+        techStack2: "css",
+        techStack3: "java",
+        techStack4: "",
+        techStack5: "",
+    }, {
+        card_Img: "assets/Proj_img/temp.jpg",
+        proj_Name: "tempName",
+        proj_Desc: "tempDesc",
+        techStack1: "html",
+        techStack2: "css",
+        techStack3: "java",
+        techStack4: "",
+        techStack5: "",
+    }, {
+        card_Img: "assets/Proj_img/temp.jpg",
+        proj_Name: "tempName",
+        proj_Desc: "tempDesc",
+        techStack1: "html",
+        techStack2: "css",
+        techStack3: "java",
+        techStack4: "",
+        techStack5: "",
+    }
+]
 
 export default projData
--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router-dom"
 import "./style.css"
 import React from 'react'
 import projData from "../../Data/ProjectData"
@@ -9,38 +8,41 @@ const Projects = () => {
     <main>
       <section>
         <h1>Projects</h1>
-        <div className="py-[5rem] w-full flex flex-col items-center flex-wrap">
+        <div className="pt-[5rem] w-full flex gap-16 md:gap-[2rem] justify-center md:justify-between flex-wrap">
           {
             projData.map((item) => {
               return (
-                  <div className="card relative block w-full md:w-[20rem] h-[20rem] rounded-xl">
-                    <img src={item.card_Img} alt="" className="card-img w-full object-scale-down max-h-[20rem]" />
-                    <div className="card-desc absolute w-full bottom-0 left-0 p-2 rounded-xl text-white bg-gradient-to-b from-transparent to-[#00000060]">
+                <a href={item.proj_Link}>
+                  <div className="card bg-orange-400">
+                    <img src={item.card_Img} alt="" className="card-img" />
+                    <div className="card-desc">
                       <p className="project-name text-3xl font-bold">
                         {item.proj_Name}
                       </p>
                       <p className="project-desc py-2 text-xl font-bold">
                         {item.proj_Desc}
                       </p>
-                      <div className="techStack pt-2 flex gap-2 font-bold">
-                        <div className={item.techStack1 ? "techstack rounded-[10px] p-1 bg-[#29292970]" : "hidden" }>
+                      <div className="pt-2 flex gap-2 font-bold">
+
+                        <div className={item.techStack1 ? "techstack" : "hidden"}>
                           {item.techStack1}
                         </div>
-                        <div className={item.techStack2 ? "techstack rounded-[10px] p-1 bg-[#29292970]" : "hidden" }>
+                        <div className={item.techStack2 ? "techstack" : "hidden"}>
                           {item.techStack2}
                         </div>
-                        <div className={item.techStack3 ? "techstack rounded-[10px] p-1 bg-[#29292970]" : "hidden" }>
+                        <div className={item.techStack3 ? "techstack" : "hidden"}>
                           {item.techStack3}
                         </div>
-                        <div className={item.techStack4 ? "techstack rounded-[10px] p-1 bg-[#29292970]" : "hidden" }>
+                        <div className={item.techStack4 ? "techstack" : "hidden"}>
                           {item.techStack4}
                         </div>
-                        <div className={item.techStack5 ? "techstack rounded-[10px] p-1 bg-[#29292970]" : "hidden" }>
+                        <div className={item.techStack5 ? "techstack" : "hidden"}>
                           {item.techStack5}
                         </div>
                       </div>
                     </div>
                   </div>
+                </a>
               )
             })
           }
