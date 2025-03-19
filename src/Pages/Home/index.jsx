05ef4cf7df8db6c30e23d962fdfc5409edb93d72
amitmishra0502 <amitmishra@iicreators.com>
2025-03-19 23:41:26 +0530

    exp

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -16,10 +16,9 @@ const Home = () => {
                     </h1>
                     <div className="text-center textFont font-black m-auto text-[12.8px] leading-6">
                     <div className="mt-4 text-justify">
-                    As a seasoned Test Engineer with 10+ years of experience in automation testing, I bring a unique blend of skepticism and technical expertise to my role. My curious mind and bug-hunting skills make me an exceptional problem solver, constantly on the lookout for flaws in software applications. 
+                    As a seasoned Test Engineer with 12+ years of experience in automation testing, I bring a unique blend of skepticism and technical expertise to my role. My curious mind and bug-hunting skills make me an exceptional problem solver, constantly on the lookout for flaws in software applications. 
                             <br />
                         </div>
-
                         <div className="mt-4 text-justify">
                             With a focus on the BFSI domain, particularly in Payment Solutions and Financial Banking, I have a deep understanding of the requirements and challenges faced by this industry. I am always seeking new opportunities to expand my skillset and make a valuable contribution to any organization. 
       <br />
@@ -28,11 +27,9 @@ const Home = () => {
                             My goal is to continue growing as a Test Engineer and become an even more productive asset for the company.
                         </div>
                     </div>
-
                     <div className="textFont mt-8">
                         Let's connect...
                     </div>
-
                     <div className=" text-lg md:text-xl gap-5 flex justify-around items-center">
                         <a target={"_blank"} href="https://www.linkedin.com/in/gaurav-singh27/">
                             <LinkedInIcon className='socialMedia' fontSize="medium" />
@@ -45,7 +42,6 @@ const Home = () => {
                             <GitHubIcon className='socialMedia' fontSize="medium" />
                         </a>
                     </div>
-
                 </section>
             </main>
         </>
